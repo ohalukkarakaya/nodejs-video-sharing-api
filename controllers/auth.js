2f7b1a9c63ec9636dea5096476c56d6fc7eed154
HalukKarakaya4js <ozgurhaluk_krky@hotmail.com>
2022-10-26 22:37:33 +0300

    auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import { createError } from "../error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
     try{
@@ -10,6 +12,31 @@ export const signup = async (req, res, next) => {
 
      await newUser.save();
      res.status(200).send("User has been created");
+    }catch(err){
+        next(err)
+    }
+}
+
+export const signin = async (req, res, next) => {
+    try{
+     const user = await User.findOne({name: req.body.name});
+     if(!user)
+        return next(createError(404, "User Not Found"));
+
+    const isCorrectPassword = await bcrypt.compare(req.body.password, user.password);
+    if(!isCorrectPassword)
+        return next(createError(400, "Wrong Password"));
+
+    const token = jwt.sign({id:user._id}, process.env.JWT);
+    const {password, ...others} = user._doc;
+
+    res
+     .cookie("access_token", token, {
+        httpOnly: true,
+     })
+      .status(200)
+      .json(others);
+
     }catch(err){
         next(err)
     }
--- models/User.js
+++ models/User.js
@@ -7,6 +7,10 @@ const UserSchema = new mongoose.Schema(
         required: true,
         unique: true
     },
+    password: {
+        type: String,
+        required: true,
+    },
     email: {
         type: String,
         required: true
