94e825c57bfada9d871c3740b278f8577fa82d0e
HalukKarakaya4js <ozgurhaluk_krky@hotmail.com>
2022-10-26 23:25:40 +0300

    update-user&delete-user

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,46 @@
-export const test = (req, res) => {
-    res.json("It is succesfull");
+import User from "../models/User.js";
+
+export const update = async (req, res, next) => {
+    if(req.params.id === req.user.id){
+        try{
+            const updatedUser = await User.findByIdAndUpdate(
+                req.params.id,
+                {
+                    $set: req.body
+                },
+                {
+                    new: true
+                }
+            );
+            res.status(200).json(updatedUser);
+        }catch(err){
+            next(err);
+        }
+    }else{
+        return next(403, "You can update just your account");
+    }
+}
+export const deleteUser = async (req, res, next) => {
+    if(req.params.id === req.user.id){
+        try{
+            await User.findByIdAndDelete(
+                req.params.id
+            );
+            res.status(200).json("User has been deleted");
+        }catch(err){
+            next(err);
+        }
+    }else{
+        return next(403, "You can delete just your account");
+    }
+}
+export const getOneUser = async (req, res, next) => {
+}
+export const subscribe = async (req, res, next) => {
+}
+export const unSubscribe = async (req, res, next) => {
+}
+export const like = async (req, res, next) => {
+}
+export const dislike = async (req, res, next) => {
 }
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,28 @@
 import express from "express";
-import {  } from "../controllers/user.js";
+import { update, deleteUser, getOneUser, subscribe, unSubscribe, like, dislike } from "../controllers/user.js";
+import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
 
+//UPDATE USER
+router.put("/:id", verifyToken, update);
+
+//DELETE USER
+router.delete("/:id", verifyToken, deleteUser);
+
+//GET ONE USER
+router.get("/find/:id", getOneUser);
+
+//SUBSCRIBE USER
+router.put("/sub/:id", verifyToken, subscribe);
+
+//UNSUBSCRİBE USER
+router.put("/unsub/:id", verifyToken, unSubscribe);
+
+//LIKE VIDEO
+router.put("/like/:videoId", verifyToken, like);
+
+//DİSLİKE VIDEO
+router.put("/dislike/:videoId", verifyToken, dislike);
 
 export default router;
