af3a027e3e1cff29df713e8d92084fd5313a9f46
HalukKarakaya4js <ozgurhaluk_krky@hotmail.com>
2022-10-26 21:09:56 +0300

    auth route

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/users.js';
 import commentRoutes from './routes/comments.js';
-import videoRoutes from './routes/comments.js';
+import videoRoutes from './routes/videos.js';
+import authRoutes from './routes/auth.js';
 
 const app = express();
 dotenv.config();
@@ -23,6 +24,7 @@ const connect = () => {
 app.use("/api/users", userRoutes);
 app.use("/api/comments", commentRoutes);
 app.use("/api/videos", videoRoutes);
+app.use("/api/auth", authRoutes);
 
 
 
