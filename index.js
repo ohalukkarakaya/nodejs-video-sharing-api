fc626dc5fa8d28530309293bdb025bb85799ad97
HalukKarakaya4js <ozgurhaluk_krky@hotmail.com>
2022-10-26 21:50:56 +0300

    auth-router

--- index.js
+++ index.js
@@ -21,10 +21,21 @@ const connect = () => {
     );
 }
 
+app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use("/api/comments", commentRoutes);
 app.use("/api/videos", videoRoutes);
-app.use("/api/auth", authRoutes);
+app.use("/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong";
+  return res.status(status).json({
+    success: false,
+    status: status,
+    message: message,
+  })
+});
 
 
 
