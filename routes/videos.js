9f5bd4ac7abd2ef759a2f1ee364c0089bfd32bc1
HalukKarakaya4js <ozgurhaluk_krky@hotmail.com>
2022-10-27 00:26:57 +0300

    video routers

--- routes/videos.js
+++ routes/videos.js
@@ -1,7 +1,31 @@
 import express from "express";
-import {  } from "../controllers/video.js";
+import { addVideo, updateVideo, deleteVideo, getVideo, addView, trend, random, sub } from "../controllers/video.js";
+import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
 
+//CREATE VİDEO
+router.post("/", verifyToken, addVideo)
+
+//UPDATE VİDEO
+router.put("/:id", verifyToken, updateVideo)
+
+//DELETE VİDEO
+router.delete("/:id", verifyToken, deleteVideo)
+
+//GET VİDEO
+router.get("/find/:id", getVideo)
+
+//INCREASE VİEW VİDEO
+router.put("/view/:id", addView)
+
+//GET TREND VİDEOS
+router.get("/trend", trend)
+
+//GET RANDOM VİDEOS
+router.get("/random", random)
+
+//GET SUBSCRİBED CHANELS VİDEOS
+router.get("/sub", verifyToken, sub)
 
 export default router;
